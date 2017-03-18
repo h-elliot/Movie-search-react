cc4a87b901572507c18abd32e711405272d485a6
h-elliot <huntercwork754@gmail.com>
2017-03-17 21:16:05 -0500

    Add imdb link button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export const Movie = (props) => {
-  let poster = props.Poster === 'N/A' ? <img src="" alt={props.Title} height="250" width="170" /> : <img src={props.Poster} alt={props.Title} height="250" width="170" />
+  let poster = props.Poster === 'N/A' ? <img src="" alt={props.Title} height="250" width="170" /> : <img src={props.Poster} alt={props.Title} height="250" width="170" />;
+  let imdbURL = `http://www.imdb.com/title/${props.imdbID}/`;
   return (
     <div className="col-md-3">
       {poster} <br/>
       {props.Title} - {props.Year}  <br/>
       {props.Plot} <br/>
+      <a href={imdbURL} target="_blank"> <button className="btn btn-sm btn-success">IMDb</button> </a>
       <a href={props.tomatoURL} target="_blank"> <button className="btn btn-sm btn-success">Rotten Tomatoes</button> </a>
       <hr />
     </div>
