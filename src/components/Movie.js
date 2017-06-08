b52b89b15b6ad8a1b7d0f4ccd0913552121962f4
h-elliot <huntercwork754@gmail.com>
2017-06-07 23:41:20 -0500

    code refactor

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React from "react";
 
-export const Movie = (props) => {
-  let poster = props.Poster === 'N/A' ? <img src="http://via.placeholder.com/170x250?text=Poster+N/A" alt={props.Title} height="250" width="170" /> : <img src={props.Poster} alt={props.Title} height="250" width="170" />;
+export const Movie = props => {
+  let poster = props.Poster === "N/A"
+    ? <img
+        src="http://via.placeholder.com/170x250?text=Poster+N/A"
+        className="img-thumbnail"
+        alt={props.Title}
+        height="250"
+        width="170"
+      />
+    : <img
+        src={props.Poster}
+        alt={props.Title}
+        className="img-thumbnail"
+        height="250"
+        width="170"
+      />;
   let imdbURL = `http://www.imdb.com/title/${props.imdbID}/`;
   return (
-    <div className="col-md-3">
-      {poster} <br/>
-      {props.Title} - {props.Year}  <br/>
-      IMDB Rating: {props.imdbRating || `N/A`} <br/>
-      Plot: {props.Plot} <br/>
-      <a href={imdbURL} target="_blank"> <button className="btn btn-sm btn-success">IMDb</button> </a>
-      <a href={props.tomatoURL} target="_blank"> <button className="btn btn-sm btn-success">Rotten Tomatoes</button> </a>
+    <div className="col-lg-3 col-md-3 col-xs-12 col-sm-6">
+      <div>
+        {poster}
+      </div>
+      <div>
+        {props.Title} - {props.Year} <br />
+      </div>
+      <div>
+        IMDB Rating: {props.imdbRating || `N/A`} <br />
+      </div>
+      <div className="plot-area">
+        Plot: {props.Plot} <br />
+      </div>
+      <div className="site-links">
+        <a href={imdbURL} target="_blank">
+          <button className="btn btn-sm btn-success">IMDb</button>{" "}
+        </a>
+        <a href={props.tomatoURL} target="_blank">
+          <button className="btn btn-sm btn-success">Rotten Tomatoes</button>
+        </a>
+      </div>
       <hr />
     </div>
   );
-}
+};
--- src/components/MovieBoard.js
+++ src/components/MovieBoard.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import {Movie} from './Movie.js';
+import React, { Component } from "react";
+import { Movie } from "./Movie.js";
+import "../App.css";
 
 class MovieBoard extends Component {
   constructor(props) {
@@ -7,38 +8,44 @@ class MovieBoard extends Component {
     // this.movies = [];
     this.state = {
       movies: []
-    }
+    };
     this.dummyMovie = {
       Poster: "N/A",
       Title: "No Results",
       Type: "",
       Year: "",
       imdbID: ""
-    }
+    };
   }
   componentWillReceiveProps(nextProps) {
     // Make the movie list as empty array when the results are not there which gives undefined object
     this.setState((prevState, props) => {
       return {
-        movies: typeof nextProps.movies !== 'undefined' ? nextProps.movies:[]
-      }
-    })
+        movies: typeof nextProps.movies !== "undefined" ? nextProps.movies : []
+      };
+    });
+
     // this.movies = typeof nextProps.movies !== 'undefined' ? nextProps.movies:[] ;
   }
+
   render() {
     //Prepare Movies to render
-    let moviesList = <div id="moviesList" className="row"> {this.state.movies.map((movie) => {
-      return (
-        <Movie {...movie} key={movie.imdbID}/>
-      );
-    })} </div>;
-      return (
-      <div className="well">
-        {moviesList}
+    let moviesList = (
+      <div id="moviesList" className="row">
+        {this.state.movies.map(movie => {
+          return <div className="movie-card"  key={movie.imdbID}><Movie {...movie}/></div>;
+        })}
+      </div>
+    );
+    return (
+      <div>
+        <hr />
+        <div>
+          {moviesList}
+        </div>
       </div>
     );
   }
 }
-
 
 export default MovieBoard;
